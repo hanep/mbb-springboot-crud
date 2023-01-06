0e2ec8ea17450bd58dab4a78fc8aa081a65ab02f
hanep <hanif@sealserver.net>
2023-01-06 14:54:04 +0800

    feat(init): crud ini

---
+++ src/main/java/com/mbbspringbootcrud/mbb/MbbApplication.java
@@ -0,0 +1,13 @@
+package com.mbbspringbootcrud.mbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbbspringbootcrud/mbb/MbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbbspringbootcrud.mbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
